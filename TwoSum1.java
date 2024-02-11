cac5b82d97a4af4f6901bb7efd352968b9b2cb0a
alamin5g <21303134@iubat.edu>
2024-02-12 03:57:44 +0600

    1. Two Sum

---
+++ TwoSum1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TwoSum1 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] arr = {1, 2, 3, 4};
+        int[] indices = twoSum(arr, 4);
+    }
+
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] pos;
+        
+
+        return pos;
+    }
+    
+}
