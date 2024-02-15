c83ff3a919e32269dda58c50fa247bf619e4bb96
alamin5g <alaminrakib4864@gmail.com>
2024-02-15 10:16:28 +0600

    pulled

--- TwoSum1.java
+++ TwoSum1.java
@@ -2,24 +2,20 @@ import java.util.HashMap;
 
 public class TwoSum1 {
 
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> hasMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             Integer numIndex = hasMap.get(nums[i]);
             if (numIndex != null) {
                 return new int[]{numIndex, i};
             }
+    
+        }
 
-<<<<<<< HEAD
-
-    public static int[] twoSum(int[] nums, int target) {
-        int[] pos = nums;
-=======
-            hasMap.put(target-nums[i], i);
-        }   
->>>>>>> 0d6cf019e397458dec11f46a56e35bd478823364
-        
         return nums;
     }
-    
+
+    public static void main(String[] args) {
+        
+    }
 }
