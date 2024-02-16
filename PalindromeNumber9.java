634271c3a854eeed0c062da907acb86cb33b3b31
alamin5g <21303134@iubat.edu>
2024-02-16 15:31:40 +0600

    palindrome number submitted again

--- PalindromeNumber9.java
+++ PalindromeNumber9.java
@@ -1,10 +1,23 @@
 public class PalindromeNumber9 {
     public boolean isPalindrome(int x) {
-        String convertedToString = Integer.toString(x);
-        if (convertedToString.equals(String.valueOf(new StringBuilder().append(convertedToString).reverse()))) {
-            return true;
+        if (x<0) {
+            return false;
+        }
+        int digitLength = (int) Math.floor(Math.log10(x)+1);
+        int msd = (int) Math.pow(10, digitLength-1);
+
+        for (int i = 0; i < (digitLength/2); i++) {
+            
+            if (x/msd != x%10) {
+                return false;
+            }
+            x = x % msd; //remove the most significant digit of x;
+            
+            x = x/10; //remove the least significant digit of x;
+            msd = msd/100;
+
         }
 
-        return false;
+        return true;
     }
 }
