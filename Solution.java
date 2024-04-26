f0e7e96cc3debe51478679167e5352e085f665d1
alamin5g <alaminrakib4864@gmail.com>
2024-04-26 12:26:23 +0600

    sum of divisor

--- Solution.java
+++ Solution.java
@@ -1,72 +1,29 @@
-class Solution {
-<<<<<<< HEAD
-
+public class Solution {
     public static void main(String[] args) {
-        
-        int max = Integer.MAX_VALUE;
-        int low = Integer.MIN_VALUE;
-        System.out.println("Out : " + reverse(1534236469));
-        System.out.println("Max : " + max);
-        System.out.println("Min : " + low);
+        int n = 3; // Sample input
+        int result = sumOfDivisorsInRange(n);
+        System.out.println("Output: " + result);
     }
 
-    public static int reverse(int x) {
-        
-        int reversedNum = doReverse(x);
-
-       return reversedNum;
-        
-    }
-
-    public static int doReverse(int actual){
-
-        long sum = 0;
-        while(actual!=0){
-            int rem = actual % 10;
-            sum = (sum * 10) + rem;
-            actual = actual/10;
-=======
-    public boolean isPalindrome(int x) {
-
-        String palindrom = Integer.toString(x);
-        int left = 0;
-        int right = palindrom.length()-1;
-
-        boolean b = palindrome(palindrom, left, right);
-
-        return b;
-        
-    }
-
-    public static boolean palindrome(String s, int left, int right){
-
-        if (left >= right) {
-            return true;
->>>>>>> 31f113f7eb843fd9d94dd506bfbeff7444d28be1
+    public static int sumOfDivisorsInRange(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += sumOfDivisors(i);
         }
-        if (isLeftRightMatched(s, left, right)) {
-            return palindrome(s, left+1, right-1);
-        }
-        
-        return false;
+        return sum;
     }
 
-<<<<<<< HEAD
-        int max = Integer.MAX_VALUE;
-        int low = Integer.MIN_VALUE;
-
-        if (low <= sum && max>= sum) {
-            return (int) sum;
-        }else{
-            return 0;
+    public static int sumOfDivisors(int i) {
+        int sum = 0;
+        for (int j = 1; j <= Math.sqrt(i); j++) {
+            if (i % j == 0) {
+                if (i / j == j) { // If divisor is same, add once
+                    sum += j;
+                } else { // Otherwise, add both divisors
+                    sum += j + i / j;
+                }
+            }
         }
-=======
-    public static boolean isLeftRightMatched(String s, int left, int right){
-        if (s.charAt(left) == s.charAt(right)) {
-            return true;
-        }
-        return false;
->>>>>>> 31f113f7eb843fd9d94dd506bfbeff7444d28be1
+        return sum;
     }
-
 }
